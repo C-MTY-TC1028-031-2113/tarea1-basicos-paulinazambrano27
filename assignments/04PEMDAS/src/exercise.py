e8795db9133458c9bfdf3d6ec06b739e23e7ea6a
paulinazambrano27 <89042787+paulinazambrano27@users.noreply.github.com>
2021-08-21 22:55:43 -0500

    Update exercise.py

--- assignments/04PEMDAS/src/exercise.py
+++ assignments/04PEMDAS/src/exercise.py
@@ -2,16 +2,24 @@ def main():
     from math import sqrt
     a=4
     b=5
-    oper1 = round((2 * (3 / 4)) + (4 * (2/3)) - (3 * (1/5)) + (5 * (1/2))
-    print (oper1)
-    oper2 = round((2 * math.sqrt((35) ** 2)) + (4 * math.sqrt((36) ** 3)) - (6 * math.sqrt(49) ** 2)) 
-    print (oper2)
-    oper3 = (a ** 3 + 2 * b ** 2) / 4 * a
-    print (oper3)
-    oper4 = ((2 * (a+b) ** 2) + (4 * (a-b) ** 2)) / (a * (b**2))
-    print (oper4)
-    oper5 = round(math.sqrt(((a+b ** 2 + 2 ** (a+b)) / (2 * a + 2 * b) ** 2))
-    print (oper5)              
+    oper1 = float((2 * (3 / 4)) + (4 * (2/3)) - (3 * (1/5)) + (5 * (1/2))
+    print (round(oper1, 4))
+                  
+    oper2 = float(2 * 35 + 4 * 6 ** 3 - 6 * 49)
+    print (round(oper2, 4))
+    a = 4
+    b = 5              
+                  
+    oper3 = float((a ** 3 + 2 * b ** 2) / (4 * a))
+    print (round(oper3, 4))
+    a = 4
+    a = 5              
+                  
+    oper4 = float((2 * (a+b) ** 2 + 4 * (a-b) ** 2) / (a * (b**2))
+    print (round(oper4, 4))
+                  
+    oper5 = float(((a + b) ** 2 + 2 ** (a+b)) ** (1/2) / (2 * a + 2 * b) ** 2)
+    print (round(oper5, 4))              
     pass
     
    
