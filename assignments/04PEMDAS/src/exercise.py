6aef5ed7a70fee2be5b416964b16bec6faf658f7
paulinazambrano27 <89042787+paulinazambrano27@users.noreply.github.com>
2021-08-22 00:54:29 -0500

    Update exercise.py

--- assignments/04PEMDAS/src/exercise.py
+++ assignments/04PEMDAS/src/exercise.py
@@ -1,7 +1,7 @@
 def main():
     from math import sqrt
    
-    oper1 = float((2 * (3 / 4)) + (4 * (2 / 3)) - (3 * (1 / 5)) + (5 * (1 / 2))
+    oper1 = float(2 * (3 / 4) + 4 * (2 / 3) - 3 * (1 / 5) + 5 * (1 / 2)
     print(round(oper1, 4))
                   
     oper2 = float(2 * 35 + 4 * 6 ** 3 - 6 * 49)
@@ -19,7 +19,7 @@ def main():
     oper4 = float((2 * (a + b) ** 2 + 4 * (a - b) ** 2) / (a * (b ** 2))
     print(round(oper4, 4))
                   
-    oper5 = float(((a + b) ** 2 + 2 ** (a + b)) ** (1 / 2) / (2 * a + 2 * b) ** 2)
+    oper5 = float(((a + b) ** 2 + 2 ** (a + b)) ** (1/2) / (2 * a + 2 * b) ** 2)
     print(round(oper5, 4))              
     pass
     
