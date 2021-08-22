a2973c8a7cf4d56f6b537d0c1fc8f7f50b48f61e
paulinazambrano27 <89042787+paulinazambrano27@users.noreply.github.com>
2021-08-22 00:48:20 -0500

    Update exercise.py

--- assignments/04PEMDAS/src/exercise.py
+++ assignments/04PEMDAS/src/exercise.py
@@ -5,20 +5,22 @@ def main():
     print (round(oper1, 4))
                   
     oper2 = float(2 * 35 + 4 * 6 ** 3 - 6 * 49)
-    print (round(oper2, 4))
+    print(round(oper2, 4))
+                  
     a = 4
     b = 5              
                   
     oper3 = float((a ** 3 + 2 * b ** 2) / (4 * a))
-    print (round(oper3, 4))
+    print(round(oper3, 4))
+                  
     a = 4
     a = 5              
                   
     oper4 = float((2 * (a + b) ** 2 + 4 * (a - b) ** 2) / (a * (b ** 2))
-    print (round(oper4, 4))
+    print(round(oper4, 4))
                   
     oper5 = float(((a + b) ** 2 + 2 ** (a + b)) ** (1 / 2) / (2 * a + 2 * b) ** 2)
-    print (round(oper5, 4))              
+    print(round(oper5, 4))              
     pass
     
    
